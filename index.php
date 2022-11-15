8c49614c08b859babd78311cc44e4dd13ec2b089
dee-u1 <62017743+dee-u1@users.noreply.github.com>
2022-11-15 14:45:10 +0800

    1st commit

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en"
+    <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="author" content="Rodelio M. Rodriguez">
+		<title>Page Replacement</title>
+
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+		<link href="assets/vendor/bootstrap4/css/bootstrap.min.css" rel="stylesheet">
+		<link href="assets/css/master.css" rel="stylesheet">
+
+    </head>
+    <body>		
+	
+	<div class="wrapper">
+        <nav id="sidebar">
+			<div class="custom-menu">
+					<strong>PR Solver</strong>&nbsp;&nbsp;&nbsp;&nbsp;
+				<button type="button" id="sidebarCollapse" class="btn btn-primary">
+				  <i class="fa fa-bars"></i>
+				  <span class="sr-only">Toggle Menu</span>
+				</button>
+			</div>
+			<div class="p-4 pt-5">
+				<h2 class="mb-4">Input</h2>
+				<form id="schedulingForm" style="margin-left: 15px; margin-right: 10px"" novalidate accept-charset="utf-8">
+					<div class="form-row">
+						<div class="form-group col-md-12">
+							<label for="Q3">Algorithm:</label>
+							<select id="algo" class="form-control form-control-sm" required>
+								<option value="LFU" selected>Least Frequently Used</option>
+								<option value="FIFO">First In First Out</option>
+								<option value="LRU">Least Recently Used</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-row">					
+						<div class="form-group col-md-12">
+							<label for="TotalFrame">Frames:</label>
+							<input type="number" class="form-control" id="TotalFrame" placeholder="" min="2" value="3" required />
+						</div>
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-12">
+							<label for="burstTimes">Pages:</label>
+							<input type="text" class="form-control form-control-sm" id="burstTimes" placeholder="e.g. 1 3 2 5 3 4 2 5 1 3 2 1 4 5 2 4" value="1 3 2 5 3 4 2 5 1 3 2 1 4 5 2 4" required>
+						</div>
+					</div>
+					<button type="submit" class="btn btn-light">Solve</button>
+				</form>           
+			</div>
+        </nav>
+
+        <div id="body" class="active">
+           
+			<h2 class="mb-4">Output</h2>
+			 
+            <div class="content">
+                <div class="container-fluid">
+					 
+					<div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="content">
+									<div class="head">
+                                        <h5 class="mb-0">Page Replacement Result</h5>
+                                    </div>
+                                     <div class="canvas-wrapper">
+                                        <div class="table-responsive">
+											<table id="Atable" border=1 cellpadding=3 cellspacing=3 width="100%" class="table table-bordered">
+									
+											</table>
+										</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+					
+					<div class="row">
+					
+						<div class="col-md-6">
+                            <div class="card">
+                                <div class="content">
+									<div class="head">
+                                        <h5 class="mb-0">Summary</h5>
+                                    </div>
+                                     <div class="canvas-wrapper">                                        
+										<ul id="summary">
+										</ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					
+						<div class="col-md-6" id="FRDiv" >
+                            <div class="card">
+                                <div class="content">
+									<div class="head">
+                                        <h5 class="mb-0">Frequency Result</h5>
+                                    </div>
+                                     <div class="canvas-wrapper">
+                                        <ul id="scores">
+										</ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						
+					</div>
+				
+					<small class="form-text">Copyright &copy; Rodelio M. Rodriguez 2022</small>
+                </div>
+            </div>
+			
+        </div>		
+    </div>
+	<script type="text/javascript" src="solver.js"></script>
+	<script src="assets/vendor/jquery3/jquery-3.4.1.min.js"></script>
+    <script src="assets/vendor/bootstrap4/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    </body>
+</html>
